d190ad1abb8c28ff3208b259ac060cf556cb5a66
James Pattison <jrpattison@live.co.uk>
2020-02-19 21:47:55 +0000

    Remove unecessary code in LandingViewModel

--- src/RepoLite/RepoLite/ViewModel/LandingViewModel.cs
+++ src/RepoLite/RepoLite/ViewModel/LandingViewModel.cs
@@ -7,38 +7,6 @@ namespace RepoLite.ViewModel
 {
     public class LandingViewModel : ViewModelBase
     {
-        private string _text;
-
-        public string Text
-        {
-            get => _text;
-            set => SetProperty(ref _text, value);
-        }
-
-        public LandingViewModel()
-        {
-            Text = @"
-using RepoLite.Common.Models;
-using System.Collections.Generic;
-
-namespace RepoLite.DataAccess
-{
-    public interface IDataSource
-    {
-        List<TableAndSchema> GetTables();
-        List<TableAndSchema> GetTables(string schema);
-        List<Table> LoadTables(List<TableAndSchema> tables);
-
-        List<string> GetProcedures();
-        List<Procedure> LoadProcedures(List<string> procedures);
-
-        List<string> GetTableColumns(Table table);
-        List<Column> LoadTableColumns(Table table);
-    }
-}
-";
-        }
-
         public ICommand NavigateToCreateModels
         {
             get
