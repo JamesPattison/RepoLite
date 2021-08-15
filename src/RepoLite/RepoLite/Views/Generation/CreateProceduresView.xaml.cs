d15e81095e3d85639ff10970666bec0e3c173541
JamesPattison <jrpattison@live.co.uk>
2021-08-15 22:33:29 +0100

    r

---
+++ src/RepoLite/RepoLite/Views/Generation/CreateProceduresView.xaml.cs
@@ -0,0 +1,22 @@
+using System.Windows.Controls;
+using System.Windows.Input;
+using RepoLite.GeneratorEngine.Models;
+using RepoLite.ViewModel.Generation;
+
+namespace RepoLite.Views.Generation
+{
+    public partial class CreateProceduresView : UserControl
+    {
+        public CreateProceduresView()
+        {
+            DataContext = IOC.Resolve<CreateProceduresViewModel>();
+            InitializeComponent();
+        }
+
+        private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e)
+        {
+            if ((sender as ListViewItem)?.DataContext is EntityToGenerate item)
+                item.Selected = !item.Selected;
+        }
+    }
+}
